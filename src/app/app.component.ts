84edec9f92446285bfb1feedc76523e5346830c3
r-vijay-r <503r.vijay@gmail.com>
2016-11-12 02:46:37 +0530

    Message service to be checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private http:Http){
+  }
+  send(number,text){
+  	let url='http://api.textlocal.in/send/?username=gmanoj.isaac@gmail.com&hash=8f26169bcfb1c0569808d67e2ff2d5f566aa1792&sender=TXTLCL&numbers=91'+number+'&message='+text;
+  	console.log(url);  	
+  	//this.http.get(url).subscribe((response)=>console.log(response));
+  }
 }
